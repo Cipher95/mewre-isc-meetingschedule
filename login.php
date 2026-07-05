9833e274533c064cb575d9744b278fcd41a0800c
Cipher95 <h.alobaidan95@gmail.com>
2026-07-05 09:26:28 +0300

    Update login.php

--- login.php
+++ login.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result->num_rows == 1) {
         $user = $result->fetch_assoc();
-        if (password_verify($password, $user['password']) || $password == 'password123') {
+        if (password_verify($password, $user['password'])) {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['username']; // Stored as username
             $_SESSION['full_name'] = $user['full_name'];
